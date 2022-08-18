3b45d96f555b992ecc9aef7f63b2432b825ae6db
sif <sifddin@hotmail.cmo>
2022-08-18 14:40:39 +0200

    update features

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,36 @@ import React , {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//internal components
+import List from './components/List'
+import AddToList from './components/AddToList';
+
+interface IState {
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string
+  }[]
+}
+
 function App() {
 
-const [people, setPeople] = useState([{
-  name: 'Green James',
-  url: "",
-  age: 25,
-  note: 'Stay in the world of legend'
-},
-{
-  name: 'Micheal Jonny',
-  url: "",
-  age: 31
-}
+const [people, setPeople] = useState<IState["people"]>([
+  {
+    name: "LeBron James",
+    url: "https://www.sophia-vereeniging.nl/app/uploads/2021/03/hamster-in-groen-bakje-scaled-e1615194439139-1200x540-c-default.jpg",
+    age: 36,
+    note: "legend in the team"
+  }
 ])
+
 
   return (
     <div className="App">
  <h1>People Invited to my Party </h1>
+ <List people={people}/>
+ <AddToList/>
     </div>
   );
 }
---
+++ src/components/AddToList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddToList = () => {
+    return(
+        <div className='AddToList'>
+        AddTodoList compponents
+         </div>
+    )
+}
+
+
+export default AddToList;
