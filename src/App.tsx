b03aecff8e58b3d4c034f219061f7d5c076c9349
sif <sifddin@hotmail.cmo>
2022-08-18 07:11:53 +0200

    update work

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React , {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const [people, setPeople] = useState([{
+  name: 'Green James',
+  url: "",
+  age: 25,
+  note: 'Stay in the world of legend'
+},
+{
+  name: 'Micheal Jonny',
+  url: "",
+  age: 31
+}
+])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <h1>People Invited to my Party </h1>
     </div>
   );
 }
